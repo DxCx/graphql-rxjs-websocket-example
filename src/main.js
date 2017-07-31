87744da6fffad252ce3b8422c88f6766dc4fea8b
Hagai Cohen <hagai.co@gmail.com>
2017-07-31 19:15:21 +0300

    chore: add hybrid mode

--- src/main.js
+++ src/main.js
@@ -1,30 +1,62 @@
-const { createServer } = require('http');
 const { SubscriptionServer } = require('subscriptions-transport-ws');
-const { executeReactive, prepareSchema, subscribe } = require('graphql-rxjs');
-const { schema } = require('./my-schema');
-prepareSchema(schema);
+const {
+  prepareSchema,
+  specifiedRules,
+  executeReactive,
+  subscribe
+} = require('graphql-rxjs');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const express = require('express');
+const { rootValue, schema } = require('./my-schema');
 
 const WS_PORT = 5000;
+// TOOGLE This to switch to hybrid mode (Http + Ws)
+const HYBRID = false;
+let graphiqlWs = '/graphiql';
 
-// Create WebSocket listener server
-const websocketServer = createServer((request, response) => {
-  response.writeHead(404);
-  response.end();
-});
-
-// Bind it to port and start listening
-websocketServer.listen(WS_PORT, () => console.log(
-  `Websocket Server is now running on http://localhost:${WS_PORT}`
-));
-
-const subscriptionServer = SubscriptionServer.create(
-  {
+const app = express();
+if ( HYBRID ) {
+  const bodyParser = require('body-parser');
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
     schema,
-    execute: executeReactive,
-    subscribe,
-  },
-  {
-    server: websocketServer,
-    path: '/graphql',
-  }
-);
+    rootValue,
+    validationRules: specifiedRules,
+  }));
+
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: `/graphql`,
+    subscriptionsEndpoint: `ws://localhost:${WS_PORT}/graphql`,
+  }));
+
+  app.use('/graphiql-plain', graphiqlExpress({
+    endpointURL: `/graphql`,
+  }));
+
+  graphiqlWs = '/graphiql-full';
+}
+
+app.use(graphiqlWs, graphiqlExpress({
+  endpointURL: `ws://localhost:${WS_PORT}/graphql`,
+  subscriptionsEndpoint: `ws://localhost:${WS_PORT}/graphql`,
+}));
+
+// Set up schema for GraphQL-RxJs
+prepareSchema(schema);
+
+const server = app.listen(WS_PORT, () => {
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      rootValue,
+      execute: executeReactive,
+      subscribe,
+      validationRules: specifiedRules,
+    },
+    {
+      server: server,
+      path: '/graphql',
+    }
+  );
+
+  console.log(`Websocket Server is now running on http://localhost:${WS_PORT}`);
+});
--- src/my-schema.js
+++ src/my-schema.js
@@ -5,6 +5,11 @@ const typeDefs = `
 # Root Subscription
 type Query {
   someLiveInt: Int
+  simpleInt: Int
+}
+
+type Subscription {
+  clock: String
 }
 `;
 
@@ -13,9 +18,20 @@ const resolvers = {
     someLiveInt(root, args, ctx) {
       return Observable.interval(100);
     },
+    simpleInt(root, args, ctx) {
+      return 10;
+    },
   },
+  Subscription: {
+    clock(root, args, ctx) {
+      return new Date().toString();
+    }
+  }
 };
 
 module.exports = {
   schema: makeExecutableSchema({typeDefs, resolvers}),
+  rootValue: {
+    clock: Observable.interval(1000).shareReplay(1),
+  }
 };
